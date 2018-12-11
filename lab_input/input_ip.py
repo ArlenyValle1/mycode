4fd3b5bc2df94451c861aba28dc1258fedea7835
ArlenyValle1 <arleny.valle@gmail.com>
2018-12-11 09:37:36 -0500

    Lab 7

---
+++ lab_input/input_ip.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+user_input = input("please enter IPv4 IP address:")
+print("You told me the IPv4 address is:" + user_input)
+#!/usr/bin/env python 3
+# Written by: Arleny
+#Collect IP address info from user, and display it back to them
+##Collect input from the user
+##Print out the input collected from the iser
+print("You told me Pv4 address is:" + user_input)
+user_input= input("Please enter the Vendor Name associated with the device:")
+print("You told me the vendor name is:" + user_input)
